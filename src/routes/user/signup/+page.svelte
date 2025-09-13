ad9d0781a34a0cd9794cd798550f12a7b5ffd3dd
LuizFXavier <zap422238@gmail.com>
2025-09-13 19:22:43 -0400

    feat: Página de sign up

--- src/routes/user/signup/+page.svelte
+++ src/routes/user/signup/+page.svelte
@@ -20,18 +20,67 @@
   <div class="nome-da-label_01"><span class="nomedalabel_01_span">Confirm Password</span></div>
 </div> -->
 
+<script lang="ts">
+  import Form from "./Form.svelte";
+</script>
+
 <main>
   <header>
     <span class = "header_span">
       Sign Up
     </span>
   </header>
+  <Form/>
+
+  <footer>
+    <button class="back_button">
+      Back
+    </button>
+    <button type="submit" class="next_button">
+      Next
+    </button>
+  </footer>
 </main>
 
 <style>
 
+footer{
+  height: 10%;
+
+  flex: 1;
+  
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin-top: 5%;
+  /* position: relative; */
+}
+
+.back_button {
+  width: 40%;
+  height: 80%;
+  margin-left: 5%;
+  background: #EF4637;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: white;
+  font-size: 2cqw;
+  font-family: Rajdhani;
+  border-radius: 14px;
+}
+
+.next_button {
+  width: 40%;
+  height: 80%;
+  margin-right: 5%;
+  background: #018BF6;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  color: white;
+  border-radius: 14px;
+  font-size: 2cqw;
+  font-family: Rajdhani;
+}
+
 header {
-  position: absolute;
   text-align: center;
   
   display: flex;
@@ -44,7 +93,7 @@ header {
 
 .header_span {
   color: white;
-  font-size: 64px;
+  font-size: 10cqw;
   font-family: Rajdhani;
   font-weight: 700;
   word-wrap: break-word;
@@ -80,17 +129,6 @@ header {
   justify-content: center;
   display: flex;
   flex-direction: column;
-}
-
-.button_01 {
-  width: 307px;
-  height: 71px;
-  left: 0px;
-  top: 0px;
-  position: absolute;
-  background: #EF4637;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 14px;
 }
 
 .options_02_span {
@@ -161,17 +199,6 @@ header {
   justify-content: center;
   display: flex;
   flex-direction: column;
-}
-
-.button_03 {
-  width: 764px;
-  height: 87px;
-  left: 79px;
-  top: 277px;
-  position: absolute;
-  background: #1F2A2C;
-  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  border-radius: 14px;
 }
 
 .placeholder_01_span {
---
+++ src/routes/user/signup/Form.svelte
@@ -0,0 +1,67 @@
+<form>
+    <div class="form_field">
+        <label for ="playerName" >
+        Player name
+        </label>
+        <input id="playerName" type="text"/>
+    </div>
+    <div class="form_field">
+        <label for ="email" >
+        E-mail
+        </label>
+        <input id="email" type="email"/>
+    </div>
+
+    <div class="form_field">
+        <label for ="password" >
+        Password
+        </label>
+        <input id="password" type="password"/>
+    </div>
+
+    <div class="form_field">
+        <label for ="password2" >
+        Confirm password
+        </label>
+        <input id="password2" type="password"/>
+    </div>
+</form>
+
+<style>
+.form_field{
+  display: flex;
+  height: 20%;
+  width: 100%;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2%;
+}
+label{
+  color: white;
+  font-size: 2cqw;
+  margin-bottom: 1%;
+  font-family: Rajdhani;
+  font-weight: 700;
+  word-wrap: break-word;
+  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+}
+input{
+  width: 50%;
+  height: 26px;
+  background: #1F2A2C;
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 10px;
+  color:white;
+}
+form {
+  margin-top: 5%;
+  width: 100%;
+  /* height: 75%; */
+  text-align: center;
+  align-items: center;
+  display: flex;
+  flex-direction: column;
+  overflow-y: auto;
+  flex: 1;
+}
+</style>
