52669200f0caef118c946483a1ec3a6fb687d51f
LuizFXavier <zap422238@gmail.com>
2025-09-13 21:45:17 -0400

    fix: Ajuste na fonte utilizada

--- src/routes/user/signup/+page.svelte
+++ src/routes/user/signup/+page.svelte
@@ -94,7 +94,7 @@ header {
 .header_span {
   color: white;
   font-size: 10cqw;
-  font-family: Rajdhani;
+  font-family: 'Rajdhani';
   font-weight: 700;
   word-wrap: break-word;
   text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
