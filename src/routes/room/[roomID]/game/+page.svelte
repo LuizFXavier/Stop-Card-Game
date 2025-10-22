99470185726533fe4d7a6338b1a44c09d73f4d2a
LuizFXavier <zap422238@gmail.com>
2025-10-21 22:17:26 -0400

    feat: Adicionado inicializador do jogo

---
+++ src/routes/room/[roomID]/game/+page.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+    import game from "$lib/refactor/Game";
+    import Mouse from "$lib/refactor/system/Mouse";
+
+    $effect(()=>{
+        game.start("gameScreen");
+    });
+
+</script>
+
+<canvas
+    id="gameScreen" 
+    onclick={(e)=>{ Mouse.clicked = e.button == 0}}
+
+    onmousemove={(e)=>{
+        Mouse.x = e.clientX;
+        Mouse.y = e.clientY;
+    }}>
+
+</canvas>
+
+<style>
+    canvas{
+        width: 100%;
+        height: 100%;
+        position:absolute;
+        top:0;
+        left:0;
+    }
+</style>
