ae9fba52f4bf577a685df727e3d17a70069b6c38
LuizFXavier <zap422238@gmail.com>
2025-11-08 19:35:07 -0400

    refactor: Modularização de componentes

--- src/routes/menu/list/Navigation.svelte
+++ src/routes/menu/list/Navigation.svelte
@@ -1,26 +1,23 @@
 <script>
 	import { goto } from "$app/navigation";
+  import SmButton from "$lib/components/SmButton.svelte";
 
 </script>
 
 <main class="container">
-    <button class="btn back_button" onclick={()=>{goto("/")}}>
-        <span class="button_span">
-            Back
-        </span>
-    </button>
 
-    <button class="btn create_button" onclick={()=>{goto("/")}}>
-        <span class="button_span">
-            Create Room
-        </span>
-    </button>
+  <SmButton color="#EF4637" onclick={()=>{goto("/")}}>
+    Back
+  </SmButton>
+  
+  <SmButton color="#9E74CE" onclick={()=>{goto("/menu/create")}}>
+    Create Room
+  </SmButton>
 
-    <button type="submit" class="btn join_button" onclick={()=>{goto("/room/2/game")}}>
-        <span class="button_span">
-            Join Private
-        </span>
-    </button>
+  <SmButton color="#36AF80" onclick={()=>{goto("/menu/join")}}>
+    Join Private
+  </SmButton>
+
 </main>
 
 <style>
@@ -31,35 +28,5 @@
     flex-direction: row;
     justify-content: space-between;
     align-items: center;
-}
-.btn{
-  box-shadow: 0px 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
-  border-radius: 1vw;
-  container-type: inline-size;
-  width: 20%;
-  height: 45%;
-  container: btn;
-  align-items: center;
-}
-
-.back_button {
-  background: #EF4637;
-}
-
-.create_button {
-  background: #9E74CE;
-}
-
-.join_button {
-  background: #36AF80;
-}
-
-.button_span{
-  color: white;
-  font-size: 2vw;
-  font-family: Rajdhani;
-  font-weight: 700;
-  word-wrap: break-word;
-  text-shadow: 0px 0.25rem 0.25rem rgba(0, 0, 0, 0.25);
 }
 </style>
