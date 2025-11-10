0aad9dee30e7c603747982f593a9da4a0581f360
LuizFXavier <zap422238@gmail.com>
2025-11-10 00:07:08 -0400

    refactor: Utilização de componente

--- src/routes/user/login/+page.svelte
+++ src/routes/user/login/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import FooterNav from "$lib/components/FooterNav.svelte";
+	import PageTitle from "$lib/components/PageTitle.svelte";
   import Form from "./Form.svelte";
 
 	function handleNext(): void {
@@ -9,11 +10,10 @@
 </script>
 
 <main>
-  <header>
-    <span class = "header_span">
-      Login
-    </span>
-  </header>
+  <PageTitle>
+    Login
+  </PageTitle>
+
   <Form/>
 
   <FooterNav labelR="Next" variant="next" onclickR={handleNext} onclickL={()=>{goto("/")}}/>
@@ -27,28 +27,6 @@ main {
     width: 100%;
     display: flex;
     flex-direction: column;
-}
-
-header {
-  text-align: center;
-  
-  display: flex;
-  flex-direction:row;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  container-type: inline-size;
-
-}
-
-.header_span {
-  color: white;
-  font-size: 10cqw;
-  font-family: 'Rajdhani';
-  font-weight: 700;
-  word-wrap: break-word;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  text-align: center;
 }
 
 </style>
--- src/routes/user/signup/+page.svelte
+++ src/routes/user/signup/+page.svelte
@@ -1,6 +1,7 @@
 <script lang="ts">
 	import { goto } from "$app/navigation";
 	import FooterNav from "$lib/components/FooterNav.svelte";
+	import PageTitle from "$lib/components/PageTitle.svelte";
 	
   import Form from "./Form.svelte";
 
@@ -11,11 +12,10 @@
 </script>
 
 <main>
-  <header>
-    <span class = "header_span">
-      Sign Up
-    </span>
-  </header>
+  <PageTitle>
+    Sign Up
+  </PageTitle>
+
   <Form/>
 
   <FooterNav labelR="Next" variant="next" onclickR={handleNext} onclickL={()=>{goto("/")}}/>
@@ -28,38 +28,6 @@ main {
     width: 100%;
     display: flex;
     flex-direction: column;
-}
-
-footer{
-  height: 10%;
-  width: 100%;
-  
-  display: flex;
-  justify-content: space-between;
-  align-items: center;
-  margin-bottom: 2%;
-  /* position: relative; */
-}
-
-header {
-  text-align: center;
-  
-  display: flex;
-  flex-direction:row;
-  text-align: center;
-  justify-content: center;
-  align-items: center;
-  container-type: inline-size;
-}
-
-.header_span {
-  color: white;
-  font-size: 10cqw;
-  font-family: 'Rajdhani';
-  font-weight: 700;
-  word-wrap: break-word;
-  text-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
-  text-align: center;
 }
 
 </style>
