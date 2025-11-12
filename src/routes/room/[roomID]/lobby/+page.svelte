941e08dcc60875b978fa0a02ba8ebbee1ec3130c
LuizFXavier <zap422238@gmail.com>
2025-11-11 22:40:55 -0400

    feat: Início do lobby

--- src/routes/room/[roomID]/lobby/+page.svelte
+++ src/routes/room/[roomID]/lobby/+page.svelte
@@ -2,11 +2,12 @@
     import type { LayoutData } from "../$types";
     import { roomEventBus } from "$lib/refactor/core/RoomEventBus";
 	import { goto } from "$app/navigation";
+	import PageTitle from "$lib/components/PageTitle.svelte";
 	
     let {data}:{data:LayoutData} = $props()
+    let roomID = data.post.roomID as string;
 
     $effect(()=>{
-        let roomID = data.post.roomID as string;
         let userID = "29"
         roomEventBus.emit('client:joinRoom', {roomID, userID})
         roomEventBus.on('server:gameStart', ()=>{
@@ -15,9 +16,13 @@
     })
 </script>
 
-<h1>
-    {data.post.roomID}
-</h1>
-<button onclick={()=>{roomEventBus.emit("client:gameStart")}}>
-    Start
-</button>
+<main class="faded_background">
+    <section class="gray_box">
+        <PageTitle>
+            Room {roomID}
+        </PageTitle>
+    </section>
+    <button onclick={()=>{roomEventBus.emit("client:gameStart")}}>
+        Start
+    </button>
+</main>
