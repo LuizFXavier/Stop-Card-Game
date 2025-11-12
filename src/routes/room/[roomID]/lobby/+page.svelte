fb34348ba5cd3f5ab078c8752d963b609bacd9c4
LuizFXavier <zap422238@gmail.com>
2025-11-12 02:28:15 -0400

    feat: Template do lobby concluído

--- src/routes/room/[roomID]/lobby/+page.svelte
+++ src/routes/room/[roomID]/lobby/+page.svelte
@@ -3,9 +3,28 @@
     import { roomEventBus } from "$lib/refactor/core/RoomEventBus";
 	import { goto } from "$app/navigation";
 	import PageTitle from "$lib/components/PageTitle.svelte";
+	import FooterNav from "$lib/components/FooterNav.svelte";
+	import PlayerList from "./PlayerList.svelte";
+	import HelpButton from "$lib/components/help/HelpButton.svelte";
+	import HelpModal from "$lib/components/help/HelpModal.svelte";
+	import RulesGrid from "./RulesGrid.svelte";
 	
-    let {data}:{data:LayoutData} = $props()
+    let {data}:{data:LayoutData} = $props();
     let roomID = data.post.roomID as string;
+
+    let playerList = $state(["Shazam 1", "Luigi"]);
+    let regras = $state([
+        { id: '6', label: '6', checked: true },
+        { id: '8', label: '8', checked: false },
+        { id: '10_1', label: '10¹', checked: false },
+        { id: 'J', label: 'J', checked: false },
+        { id: 'J_2', label: '', checked: false },
+        { id: '7', label: '7', checked: false },
+        { id: '9', label: '9', checked: false },
+        { id: '10_2', label: '10²', checked: false },
+        { id: 'Q', label: 'Q', checked: false },
+        { id: 'A', label: 'A', checked: false }
+    ]);
 
     $effect(()=>{
         let userID = "29"
@@ -14,15 +33,35 @@
             goto( `/room/${roomID}/game`)
         })
     })
+
+    function handleStart(){
+        roomEventBus.emit("client:gameStart")
+    }
+    function handleBack(){
+        goto("/menu/list")
+    }
 </script>
 
 <main class="faded_background">
-    <section class="gray_box">
+    <HelpButton/>
+    <HelpModal/>
+    <section class="gray_box container">
         <PageTitle>
             Room {roomID}
         </PageTitle>
+        <PlayerList {playerList}/>
+        <RulesGrid {regras}/>
+        <FooterNav onclickL={handleBack} onclickR={handleStart} variant="next" labelR="Start"/>
     </section>
-    <button onclick={()=>{roomEventBus.emit("client:gameStart")}}>
-        Start
-    </button>
+    
 </main>
+
+<style>
+.container{
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  gap:2.5rem;
+}
+</style>
---
+++ src/routes/room/[roomID]/lobby/RulesGrid.svelte
@@ -0,0 +1,32 @@
+<script lang="ts">
+	import Checkbox from "$lib/components/Checkbox.svelte";
+
+    let {regras = $bindable()} = $props();
+
+</script>
+
+<div class="rules-grid">
+      {#each regras as regra (regra.id)}
+        <Checkbox bind:checked={regra.checked} disabled={true}>
+          {#if regra.label.length > 0}
+            {regra.label}
+          {:else}
+            <img src="/icons/joker_ic.png" alt="Joker" class="joker-icon"/>
+          {/if}
+        </Checkbox>
+      {/each}
+</div>
+
+<style>
+.rules-grid {
+display: grid;
+grid-template-columns: repeat(5, 1fr);
+gap: 1rem 2rem; /* Gap vertical e horizontal */
+
+}
+.joker-icon{
+  height: 1em;
+  width: auto;
+  vertical-align: middle;      
+}
+</style>
